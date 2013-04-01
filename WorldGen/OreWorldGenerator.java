75b057d335a2887e5ce2a7ff8e3c6cb913c9de40
Alexei <aksl@bk.ru>
2013-04-01 23:21:01 +0400

    Update OreWorldGenerator.java
    I made previous version of the code much more lighter.

--- WorldGen/OreWorldGenerator.java
+++ WorldGen/OreWorldGenerator.java
@@ -16,21 +16,9 @@ public class OreWorldGenerator implements IWorldGenerator {
 	public void generate(Random random, int chunkX, int chunkZ, World world,
 			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
 		switch (world.provider.dimensionId) {
-        case 1:
-            generateNether(world, random, chunkX * 16, chunkZ * 16);
         case 0:
             generateSurface(world, random, chunkX * 16, chunkZ * 16);
-        case -1:
-            generateEnd(world, random, chunkX * 16, chunkZ * 16);
 		}
-	}
-
-	private void generateEnd(World world, Random random, int i, int j) {
-		
-	}
-
-	private void generateNether(World world, Random random, int i, int j) {
-		
 	}
 
 	private void generateSurface(World world, Random random, int chunkX, int chunkZ) {
